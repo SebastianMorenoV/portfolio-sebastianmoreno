3a0460451f5dfdf037a3032cde1c96b69894c5f7
SebastianMoreno <erick.moreno252840@potros.itson.edu.mx>
2026-04-24 00:45:41 -0700

    mmm again

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/portfolio-sebastianmoreno/",
+  build: {
+    outDir: "docs", // <--- This tells Vite to name the folder "docs"
+  },
 });
